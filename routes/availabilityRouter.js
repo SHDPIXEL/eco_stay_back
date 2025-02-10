2b29d601945c648eb25efe7cff1488d3b3480590
SHDPIXEL <shdpixel@gmail.com>
2025-02-10 11:36:42 +0530

    Update availabilityRouter.js

--- routes/availabilityRouter.js
+++ routes/availabilityRouter.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { getAvailabilityByRoomId } = require("../controllers/availabilityController");
 
-router.get('/availability/:roomId',getAvailabilityByRoomId)
+router.post('/availability/:roomId',getAvailabilityByRoomId)
 
 module.exports = router;
