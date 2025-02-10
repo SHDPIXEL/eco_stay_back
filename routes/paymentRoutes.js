9bdcc42727f91a66c42be3c77f35113cb1397312
Shiv Gadway <47748187+shivg03@users.noreply.github.com>
2025-02-10 15:09:31 +0530

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -4,11 +4,15 @@ const {
   createPayment,
   getAllPayments,
   getPaymentById,
+  getPaymentsByUserId,
+  getPaymentsByAgentId
 
 } = require('../controllers/paymentController');
 
 router.post('/payment', createPayment);
 router.get('/payment', getAllPayments);
 router.get('/payment/:id', getPaymentById);
+router.get('/payment/user/:user_id', getPaymentsByUserId);
+router.get('/payment/agent/:agentId', getPaymentsByAgentId);
 
 module.exports = router;
