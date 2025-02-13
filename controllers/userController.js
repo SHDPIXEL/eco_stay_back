42845325fc37443979af36642dfbb24ac2a769a5
Shiv Gadway <47748187+shivg03@users.noreply.github.com>
2025-02-13 15:47:03 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -107,23 +107,23 @@ const getUserByEmail = async (req, res) => {
         const user = await User.findOne({
           where: { email }, // ✅ Correct usage
           attributes: ['id', 'name', 'idProof', 'phone', 'address', 'city', 'state', 'country', 'pincode', 'status'],
-          logging: console.log,
         });
-        // Log the fetched user for debugging
-        console.log('Fetched User:', user);
+       return res.status(400).json(user);
+        // // Log the fetched user for debugging
+        // console.log('Fetched User:', user);
 
-        if (!user) {
-            res.status(404).json({ message: 'User not found' });
-        }
+        // if (!user) {
+        //     res.status(404).json({ message: 'User not found' });
+        // }
 
-        // Generate JWT token
-        const token = jwt.sign(
-            { userId: user.id, userName: user.name },
-            process.env.JWT_SECRET,
-            { expiresIn: "1h" }
-        );
+        // // Generate JWT token
+        // const token = jwt.sign(
+        //     { userId: user.id, userName: user.name },
+        //     process.env.JWT_SECRET,
+        //     { expiresIn: "1h" }
+        // );
 
-        return res.status(200).json({ message: 'User logged in successfully', user, token });
+        // return res.status(200).json({ message: 'User logged in successfully', user, token });
     } catch (error) {
         console.error('Error fetching user by email:', error);
         return res.status(500).json({ message: 'Internal server error' });
